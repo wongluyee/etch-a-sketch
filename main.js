5f6e0c480aa1ce4eb7e0c603a906ac8b25ec0ff2
luyeewong <wongluyee1015@hotmail.com>
2023-01-02 19:19:43 +0900

    Add hover effect, when mouseover, cell become black. When mouseout, cell become white.

--- main.js
+++ main.js
@@ -1,12 +1,31 @@
 const container = document.getElementById("container");
 
-function makeRows(rows, columns) {
+function makeGrid(rows, columns) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-columns', columns);
   for (c = 0; c < (rows * columns); c++) {
     let cell = document.createElement("div");
-    container.appendChild(cell).className = "grid-item";
+    container.appendChild(cell).className = "grid";
   };
 };
 
-makeRows(16, 16);
+makeGrid(3, 3);
+
+function fillCell(e){
+    e.target.style.backgroundColor = "#000000";
+}
+
+function removeCellColor(e){
+    e.target.style.backgroundColor = "#FFFFFF";
+}
+
+function draw (){
+    const cells = document.querySelectorAll(".grid");
+
+    cells.forEach((cell) => {
+        cell.addEventListener('mouseover', fillCell);
+        cell.addEventListener('mouseout', removeCellColor);
+    });
+}
+
+draw();
