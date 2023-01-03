f51598cdf17b33686a4e6fd588616b8e5f8317d7
luyeewong <wongluyee1015@hotmail.com>
2023-01-03 15:18:07 +0900

    Add buttons to change grid and clear grid

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const container = document.getElementById("container");
+const changeGridBtn = document.getElementsByClassName("change-grid");
 
-function makeGrid(rows, columns) {
+function makeGrid(rows = 16, columns = 16) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-columns', columns);
   for (c = 0; c < (rows * columns); c++) {
@@ -9,14 +10,10 @@ function makeGrid(rows, columns) {
   };
 };
 
-makeGrid(3, 3);
+makeGrid();
 
 function fillCell(e){
     e.target.style.backgroundColor = "#000000";
-}
-
-function removeCellColor(e){
-    e.target.style.backgroundColor = "#FFFFFF";
 }
 
 function draw (){
@@ -24,8 +21,37 @@ function draw (){
 
     cells.forEach((cell) => {
         cell.addEventListener('mouseover', fillCell);
-        cell.addEventListener('mouseout', removeCellColor);
     });
 }
 
 draw();
+
+//Remove all the cells for changing grid
+function removeGrid(){
+    const cells = document.querySelectorAll(".grid");
+
+    cells.forEach((cell) => {
+        cell.remove();
+    });
+}
+
+//Remove all the cells first, then create a new one.
+//draw() function in the end so that user can draw on the new grid.
+function changeGrid(){
+    let input = prompt("Please enter the new grid.");
+
+    //So that if user click cancel, the grid would not disappear
+    if (input === null) return;
+
+    removeGrid();
+    makeGrid(input,input);
+    draw();
+}
+
+function clearGrid(){
+    const cells = document.querySelectorAll(".grid");
+
+    cells.forEach((cell) => {
+        cell.style.backgroundColor = "#FFFFFF";
+    });
+}
